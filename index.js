aa37d42160e49f4e2089d2e9a7a2a3df1ee20319
limasuu <szlima.93@gmail.com>
2022-10-04 12:36:30 -0300

    Definindo o estado inicial da aplicação

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
-const body= document.querySelector('body');
-const botaoTema= document.querySelector('#botaoTema');
-const componentes= document.querySelectorAll('.componente');
 
 const definirTema= tema => {   
+    const body= document.querySelector('body');
+    const botaoTema= document.querySelector('#botaoTema');
+    const componentes= document.querySelectorAll('.componente');
+
     body.style.background= tema.fundo;
     body.style.color= tema.padrao;
 
@@ -13,3 +14,13 @@ const definirTema= tema => {
     for(let i=0; i<componentes.length; i++)
         componentes[i].style.borderColor= tema.destaque;
 };
+
+//Estado inicial da aplicação
+const estadoPadrao= {
+    fundo: 'rgb(255, 245, 233)',
+    padrao: 'rgb(105, 110, 110)',
+    destaque: 'rgb(228, 95, 172)',
+    icone: 'fa-moon'
+};
+
+definirTema(estadoPadrao);
